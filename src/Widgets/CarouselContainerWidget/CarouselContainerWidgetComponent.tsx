4f08ab4b2816c8e098dedadeb12d23223979c9c9
Alexander Pepper <alexander.pepper@justrelate.com>
2023-06-15 10:26:15 +0200

    Pretteri

--- src/Widgets/CarouselContainerWidget/CarouselContainerWidgetComponent.tsx
+++ src/Widgets/CarouselContainerWidget/CarouselContainerWidgetComponent.tsx
@@ -12,14 +12,18 @@ Scrivito.provideComponent(CarouselContainerWidget, ({ widget }) => {
           className={itemClassName(item.get('backgroundColor'))}
         >
           <Scrivito.InPlaceEditingOff>
-            <Scrivito.ImageTag content={item} attribute="background" className="img-background"/>
+            <Scrivito.ImageTag
+              content={item}
+              attribute="background"
+              className="img-background"
+            />
           </Scrivito.InPlaceEditingOff>
-          <div className='container'>
-          <Scrivito.ContentTag
-            content={item}
-            attribute="caption"
-            className="carousel-caption"
-          />
+          <div className="container">
+            <Scrivito.ContentTag
+              content={item}
+              attribute="caption"
+              className="carousel-caption"
+            />
           </div>
         </Carousel.Item>
       ))}
