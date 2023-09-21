6ff987d3d448fe087c7ce12b35a4dc3caea58077
Alexander Pepper <alexander.pepper@justrelate.com>
2023-09-21 09:39:16 +0200

    Fake a picture

--- src/Data/CurrentUser/CurrentUserDataItem.ts
+++ src/Data/CurrentUser/CurrentUserDataItem.ts
@@ -8,5 +8,8 @@ export const CurrentUserDataItem = provideDataItem('CurrentUser', async () => {
     jrUserId: user.id(),
     name: user.name(),
     email: user.email(),
+    picture:
+      'https://secure.gravatar.com/avatar/45b31509c11165be5fac1dd17b76accf?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fap.png',
+    // picture: '',
   }
 })
