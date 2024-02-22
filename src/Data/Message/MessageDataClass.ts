b0551ebebca108f01cdf8ccae67b65491c9ca76f
Alexander Pepper <alexander.pepper@justrelate.com>
2024-02-22 13:23:42 +0100

    Connect "Message" to PisaSales

--- src/Data/Message/MessageDataClass.ts
+++ src/Data/Message/MessageDataClass.ts
@@ -1,40 +1,40 @@
+import { provideDataClass, unstable_JrRestApi } from 'scrivito'
 import { convertBlobAttributes } from '../../utils/convertBlobAttributes'
-import { provideLocalStorageDataClass } from '../../utils/provideLocalStorageDataClass'
 
-export const Message = provideLocalStorageDataClass('Message', {
-  prepareData: async (data) => {
-    const newData = await convertBlobAttributes(data)
+const apiPath = '../pisa-api/message'
 
-    return {
-      ...newData,
-      createdBy: newData.createdBy || 'F87BDC400E41D630E030A8C00D01158A',
-      createdAt: newData.createdAt || new Date().toISOString(),
-    }
+// TODO: use `provideDataClass('Message', { apiPath })` once available (with 1.39.0?)
+export const Message = provideDataClass('Message', {
+  connection: {
+    index: (params) =>
+      unstable_JrRestApi.fetch(apiPath, {
+        params: {
+          ...params.filters(),
+          _continuation: params.continuation(),
+          _order: params.order().length
+            ? params
+                .order()
+                .map((o) => o.join('.'))
+                .join(',')
+            : undefined,
+          _search: params.search() || undefined,
+        },
+      }) as Promise<{ results: Array<{ _id: string }>; continuation?: string }>,
+
+    get: (id) => unstable_JrRestApi.fetch(`${apiPath}/${id}`),
+    create: async (data) =>
+      unstable_JrRestApi.fetch(apiPath, {
+        method: 'post',
+        data: await convertBlobAttributes(data),
+      }) as Promise<{
+        _id: string
+      }>,
+    update: async (id, data) =>
+      unstable_JrRestApi.fetch(`${apiPath}/${id}`, {
+        method: 'patch',
+        data: await convertBlobAttributes(data),
+      }),
+    delete: (id) =>
+      unstable_JrRestApi.fetch(`${apiPath}/${id}`, { method: 'delete' }),
   },
-  initialContent: [
-    {
-      _id: '5095866DEFF74DCDBE56B016898137491',
-      subjectId: '5095866DEFF74DCDBE56B01689813749',
-      text: 'Sorry to hear. Did you try turning it off and back on again?',
-      createdBy: 'D456ACF6FF405922E030A8C02A010C68',
-      createdAt: '2024-01-12T09:39:04Z',
-      attachments: [],
-    },
-    {
-      _id: '5095866DEFF74DCDBE56B016898137492',
-      subjectId: '5095866DEFF74DCDBE56B01689813749',
-      text: "I just tried it. Now it's working again. Thanks!",
-      createdBy: 'F87BDC400E41D630E030A8C00D01158A',
-      createdAt: '2024-01-12T09:39:29Z',
-      attachments: [],
-    },
-    {
-      _id: '5095866DEFF74DCDBE56B016898137493',
-      subjectId: '5095866DEFF74DCDBE56B01689813749',
-      text: 'Great to hear',
-      createdBy: 'D456ACF6FF405922E030A8C02A010C68',
-      createdAt: '2024-01-12T09:39:49Z',
-      attachments: [],
-    },
-  ],
 })
