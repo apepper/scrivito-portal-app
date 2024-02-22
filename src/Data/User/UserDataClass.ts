f046077d4f7d91b94023b27c04b0f9c9de57e379
Alexander Pepper <alexander.pepper@justrelate.com>
2024-02-22 13:23:39 +0100

    Connect "User" to PisaSales

--- src/Data/User/UserDataClass.ts
+++ src/Data/User/UserDataClass.ts
@@ -1,96 +1,54 @@
-import { provideLocalStorageDataClass } from '../../utils/provideLocalStorageDataClass'
-import richterImage from './FakeBinaries/richter.jpg'
-import braschauImage from './FakeBinaries/braschau.jpg'
 import { CurrentUser } from '../CurrentUser/CurrentUserDataItem'
-import { load } from 'scrivito'
+import { load, provideDataClass, unstable_JrRestApi } from 'scrivito'
 
-export const User = provideLocalStorageDataClass('User', {
-  postProcessData: async (data) => {
-    // Use the data item cached by Scrivito instead of connecting to the backend directly
-    const pisaUserId = await load(() => CurrentUser.get('pisaUserId'))
-    if (data._id !== pisaUserId) return data
+const apiPath = '../pisa-api/user'
 
-    return {
-      _id: data._id,
-      staff: false,
-      email: await load(() => CurrentUser.get('email')),
-      familyName: await load(() => CurrentUser.get('familyName')),
-      givenName: await load(() => CurrentUser.get('givenName')),
-      image: { url: await load(() => CurrentUser.get('picture')) },
-      name: await load(() => CurrentUser.get('name')),
-      salutation: await load(() => CurrentUser.get('salutation')),
-    }
+// TODO: use `provideDataClass('User', { apiPath })` once available (with 1.39.0?)
+export const User = provideDataClass('User', {
+  connection: {
+    index: async (params) => {
+      const { results, continuation } = await (unstable_JrRestApi.fetch(
+        apiPath,
+        {
+          params: {
+            ...params.filters(),
+            _continuation: params.continuation(),
+            _order: params.order().length
+              ? params
+                  .order()
+                  .map((o) => o.join('.'))
+                  .join(',')
+              : undefined,
+            _search: params.search() || undefined,
+          },
+        },
+      ) as Promise<{ results: Array<{ _id: string }>; continuation?: string }>)
+
+      return {
+        results: await Promise.all(results.map(postProcessData)),
+        continuation,
+      }
+    },
+    get: async (id) => {
+      const item = await unstable_JrRestApi.fetch(`${apiPath}/${id}`)
+      return item ? postProcessData(item as { _id: string }) : item
+    },
   },
-  initialContent: [
-    {
-      _id: '052601BEBCEC39C8E040A8C00D0107AC',
-      name: 'Thomas Richter',
-      salutation: 'Herr',
-      givenName: 'Thomas',
-      familyName: 'Richter',
-      staff: true,
-      position: 'Vertriebsmanager',
-      email: 't.richter@example.com',
-      image: { url: richterImage },
-    },
-    {
-      _id: '18EEEAEC56D37397E040A8C00F012319',
-      name: 'Adrian Schärli',
-      salutation: 'Herr',
-      givenName: 'Adrian',
-      familyName: 'Schärli',
-      staff: false,
-    },
-    {
-      _id: '228C5C6067EF486FB72F8D4BAAE6AB08',
-      name: 'Olaf Behrends',
-      salutation: 'Herr',
-      givenName: 'Olaf',
-      familyName: 'Behrends',
-      staff: false,
-    },
-    {
-      _id: 'D38D7FBFA277856DE030A8C02A010460',
-      name: 'Frank Goosens',
-      salutation: 'Herr',
-      givenName: 'Frank',
-      familyName: 'Goosens',
-      staff: false,
-    },
-    {
-      _id: 'D456ACF6FF405922E030A8C02A010C68',
-      name: 'Benjamin Braschau',
-      salutation: 'Herr',
-      givenName: 'Benjamin',
-      familyName: 'Braschau',
-      staff: true,
-      position: 'Serviceleiter',
-      email: 'b.braschau@example.com',
-      image: { url: braschauImage },
-    },
-    {
-      _id: 'F87BDC400E41D630E030A8C00D01158A',
-      name: 'Markus Neumann',
-      salutation: 'Herr',
-      givenName: 'Markus',
-      familyName: 'Neumann',
-      staff: false,
-    },
-    {
-      _id: 'F9E1B255870E5EEEE030A8C00D012D39',
-      name: 'Heinrich Siebert',
-      salutation: 'Herr',
-      givenName: 'Heinrich',
-      familyName: 'Siebert',
-      staff: false,
-    },
-    {
-      _id: '4668C6ADEF0443BE80FB4049097A901A',
-      name: 'Administrator',
-      salutation: '',
-      givenName: '',
-      familyName: 'Administrator',
-      staff: true,
-    },
-  ],
 })
+
+async function postProcessData(data: { _id: string }) {
+  // Use the data item cached by Scrivito instead of connecting to the backend directly
+  const pisaUserId = await load(() => CurrentUser.get('pisaUserId'))
+  if (data._id !== pisaUserId) return data
+
+  return {
+    _id: data._id,
+    staff: false,
+    email: await load(() => CurrentUser.get('email')),
+    familyName: await load(() => CurrentUser.get('familyName')),
+    givenName: await load(() => CurrentUser.get('givenName')),
+    image: { url: await load(() => CurrentUser.get('picture')) },
+    name: await load(() => CurrentUser.get('name')),
+    salutation: await load(() => CurrentUser.get('salutation')),
+  }
+}
