00e535e6b530bcf5363d219bc641676e0d3e07f5
Alexander Pepper <alexander.pepper@justrelate.com>
2025-07-03 14:40:15 +0200

    Always show "restoreContent" command

--- src/Extensions/RestoreContent/RestoreContentEditingConfig.ts
+++ src/Extensions/RestoreContent/RestoreContentEditingConfig.ts
@@ -1,9 +1,7 @@
-import { currentPageParams, extendMenu, canWrite, openDialog } from 'scrivito'
+import { extendMenu, canWrite, openDialog } from 'scrivito'
 import icon from 'bootstrap-icons/icons/stars.svg'
 
 extendMenu((menu) => {
-  if (currentPageParams().restoreContent !== null) return
-
   menu.insert({
     enabled: canWrite(),
     icon,
