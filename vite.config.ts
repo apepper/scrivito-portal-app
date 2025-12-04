761030dbcdb5e91702a66377ac50eab9d4ded3b6
Alexander Pepper <alexander.pepper@justrelate.com>
2025-12-04 16:58:44 +0100

    [vite] Properly dedupe react and react-dom

--- vite.config.ts
+++ vite.config.ts
@@ -90,6 +90,7 @@ export default defineConfig(({ mode }) => {
           ? '@honeybadger-io/js'
           : resolve(__dirname, 'src/honeybadgerStub.ts'),
       },
+      dedupe: ['react', 'react-dom'],
     },
     server: {
       port: 8080,
