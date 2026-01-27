1f7e65f4f4964a83bf49909062c656039f703549
Alexander Pepper <alexander.pepper@justrelate.com>
2026-01-27 10:22:02 +0100

    TPM: Use provideDataService

--- src/Data/index.ts
+++ src/Data/index.ts
@@ -1,3 +1,7 @@
+import { provideDataService } from 'scrivito'
+
 import.meta.glob(['./**/*DataClass.ts', './**/*DataItem.ts'], { eager: true })
+
+provideDataService('https://web102.crm.pisasales.de/api/portal')
 
 export {}
